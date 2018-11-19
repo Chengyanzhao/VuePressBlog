4c2fc78ba28f81fa8ed4eaf75a21f9bc50b19832
bug Maker <451354@qq.com>
2018-11-19 08:54:52 +0800

    修改：day显示错误的bug

--- docs/.vuepress/components/year-progress.vue
+++ docs/.vuepress/components/year-progress.vue
@@ -61,7 +61,7 @@ export default {
       const second = (date.getSeconds()).toString().padStart(2, '0')
       const minute = (date.getMinutes()).toString().padStart(2, '0')
       const hour = (date.getHours()).toString().padStart(2, '0')
-      const day = (date.getDay() + 1).toString().padStart(2, '0')
+      const day = date.getDate().toString().padStart(2, '0')
       const month = (date.getMonth() + 1).toString().padStart(2, '0')
       const year = (date.getFullYear()).toString().padStart(2, '0')
       const nextTime = {
