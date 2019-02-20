21ef3c6abdf26f40b8399b680ba9f3afdfc184ff
bug Maker <451354@qq.com>
2019-02-20 14:21:50 +0800

    修改：博客导航。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,19 +17,20 @@ module.exports = {
       { text: '首页', link: '/' },
       {
         text: '博客',
-        items: [
-          { text: 'HTML', link: '/blog/HTML/' },
-          { text: 'JavaScript', link: '/blog/JavaScript/' },
-          { text: 'CSS', link: '/blog/CSS/' },
-          { text: 'DOM', link: '/blog/DOM/' },
-          { text: '前端', link: '/blog/FE/' },
-          { text: 'Vue', link: '/blog/Vue/' },
-          { text: '部署', link: '/blog/Deploy/' },
-          { text: '正则表达式', link: '/blog/Regexp/' },
-          { text: '兼容性', link: '/blog/Compatibility/' },
-          { text: '操作系统', link: '/blog/System/' },
-          { text: '其他', link: '/blog/Other/' }
-        ]
+        link: '/blog/',
+        // items: [
+        //   { text: 'HTML', link: '/blog/HTML/' },
+        //   { text: 'JavaScript', link: '/blog/JavaScript/' },
+        //   { text: 'CSS', link: '/blog/CSS/' },
+        //   { text: 'DOM', link: '/blog/DOM/' },
+        //   { text: '前端', link: '/blog/FE/' },
+        //   { text: 'Vue', link: '/blog/Vue/' },
+        //   { text: '部署', link: '/blog/Deploy/' },
+        //   { text: '正则表达式', link: '/blog/Regexp/' },
+        //   { text: '兼容性', link: '/blog/Compatibility/' },
+        //   { text: '操作系统', link: '/blog/System/' },
+        //   { text: '其他', link: '/blog/Other/' }
+        // ]
       },
       // {
       //   text: '专栏',
@@ -41,70 +42,116 @@ module.exports = {
       { text: '闲扯', link: '/chitchat/' },
       { text: '关于', link: '/about/' }
     ],
-    sidebarDepth: 2,
-    displayAllHeaders: true,
+    // sidebarDepth: 2,
+    lastUpdated: 'Last Updated',
+    displayAllHeaders: false,
     // 侧边栏
     sidebar: {
-      '/blog/HTML/': [{
-        title: 'HTML',
-        collapsable: false,
-        children: ['', 'history-api']
-      }],
-      '/blog/JavaScript/': [{
-        title: 'JavaScript',
-        collapsable: false,
-        children: ['', 'jsonp', 'hoisting', 'modules', 'array-unique', 'promise', 'prototype and constructor', 'inherit']
-      }],
-      '/blog/CSS/': [{
-        title: 'CSS',
-        collapsable: true,
-        children: ['', 'pseudo', 'font-face', 'background', 'border', 'gradient', 'transform', 'transition']
-      }],
-      '/blog/DOM/': [{
-        title: 'DOM',
-        collapsable: false,
-        children: ['', 'attribute and property']
-      }],
-      '/blog/FE/': [{
-        title: '前端',
-        collapsable: false,
-        children: ['', 'browser-node-v8']
-      }],
-      '/blog/Vue/': [{
-        title: 'Vue记录',
-        collapsable: false,
-        children: ['', 'Record', 'history-api']
-      }],
-      '/blog/MongoDB/': [{
-        title: 'MongoDB',
-        collapsable: false,
-        children: ['']
-      }],
-      '/blog/Deploy/': [{
-        title: '部署',
-        collapsable: false,
-        children: ['', 'nssm', 'nginx']
-      }],
-      '/blog/Regexp/': [{
-        title: '部署',
-        collapsable: false,
-        children: ['', 'common']
-      }],
-      '/blog/Compatibility/': [{
-        title: '兼容性',
-        collapsable: false,
-        children: ['']
-      }],
-      '/blog/System/': [{
-        title: '操作系统',
-        collapsable: false,
-        children: ['', 'macOS']
-      }],
-      '/blog/Other/': [{
-        title: '其他',
-        collapsable: false,
-        children: ['', 'ali-oss']
-      }],
+      '/blog/': [
+        '/blog/',
+        // html
+        {
+          title: 'HTML',
+          collapsable: true,
+          children: ['/blog/HTML/history-api']
+        },
+        // js
+        {
+          title: 'JavaScript',
+          children: [
+            '/blog/JavaScript/jsonp',
+            '/blog/JavaScript/hoisting',
+            '/blog/JavaScript/modules',
+            '/blog/JavaScript/array-unique',
+            '/blog/JavaScript/promise',
+            '/blog/JavaScript/prototype and constructor',
+            '/blog/JavaScript/inherit']
+        },
+        // css
+        {
+          title: 'CSS',
+          children: [
+            '/blog/CSS/pseudo',
+            '/blog/CSS/font-face',
+            '/blog/CSS/background',
+            '/blog/CSS/border',
+            '/blog/CSS/gradient',
+            '/blog/CSS/transform',
+            '/blog/CSS/transition']
+        },
+        // dom
+        {
+          title: 'DOM',
+          children: [
+            '/blog/DOM/attribute and property'
+          ]
+        },
+        // fe
+        {
+          title: '前端',
+          children: [
+            '/blog/FE/browser-node-v8'
+          ]
+        },
+        // vue
+        {
+          title: 'Vue',
+          children: [
+            '/blog/Vue/Record',
+            '/blog/Vue/history-api'
+          ]
+        },
+        // react
+        // {
+        //   title: 'React',
+        //   children: [
+        //     '/blog/React/'
+        //   ]
+        // },
+        // mongodb
+        // {
+        //   title: 'MongoDB',
+        //   children: [
+        //     '/blog/MongoDB/'
+        //   ]
+        // },
+        // Deploy
+        {
+          title: '部署',
+          children: [
+            '/blog/Deploy/nssm',
+            '/blog/Deploy/nginx'
+          ]
+        },
+        // regexp
+        // {
+        //   title: '正则表达式',
+        //   children: [
+        //     '/blog/Regexp/common'
+        //   ]
+        // }
+        // Compatibility
+        // {
+        //   title: '兼容性',
+        //   children: [
+        //     '/blog/Compatibility/'
+        //   ]
+        // }
+        // os
+        {
+          title: '操作系统',
+          children: [
+            '/blog/System/macOS'
+          ]
+        },
+        // others
+        {
+          title: '其他',
+          children: [
+            '/blog/Other/ali-oss'
+          ]
+        }
+      ],
       '/paino/': [{
         title: '钢琴',
         collapsable: false,
